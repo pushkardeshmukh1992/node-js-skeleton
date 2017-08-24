5da3f039f20f9b53d103c167ab993a115d896e76
Pushkar Deshmukh <pushkard@sminq.com>
2017-08-24 09:34:19 +0530

    set buildpack to node js

--- config/db.js
+++ config/db.js
@@ -9,6 +9,7 @@ var config = {
   "port": 27017
 };
 
+
 var port = (config.port.length > 0) ? ":" + config.port : '';
 var login = (config.user.length > 0) ? config.user + ":" + config.pw + "@" : '';
 // var uristring =  process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||  "mongodb://" + login + config.host + port + "/" + config.db;
