d9d702d5d017c8346d42785821c63488b91f5af3
Pushkar Deshmukh <pushkard@sminq.com>
2017-08-24 09:26:03 +0530

    updated db config to mongolab url

--- config/db.js
+++ config/db.js
@@ -11,7 +11,8 @@ var config = {
 
 var port = (config.port.length > 0) ? ":" + config.port : '';
 var login = (config.user.length > 0) ? config.user + ":" + config.pw + "@" : '';
-var uristring =  process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||  "mongodb://" + login + config.host + port + "/" + config.db;
+// var uristring =  process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||  "mongodb://" + login + config.host + port + "/" + config.db;
+var uristring = 'mongodb://sushantj:2485001s@ds153853.mlab.com:53853';
 
 var mongoOptions = { db: { safe: true } };
 
